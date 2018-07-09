66ef252b5040458702236d9b99e5e2a0ab21f66c
jdayssol <jdayssol@effisoft-group.com>
2018-07-09 20:47:21 +0200

    POJO class Tile

--- src/com/otto/ottorobot/Tile.java
+++ src/com/otto/ottorobot/Tile.java
@@ -1,11 +1,16 @@
 package com.otto.ottorobot;
 
 public class Tile {
-	boolean isBlocked;
+	private boolean isBlocked;
+
 	public Tile(boolean isBlocked) {
 		this.isBlocked = isBlocked;
 	}
 	public boolean isBlocked() {
 		return isBlocked;
-	}		
+	}	
+	
+	public void setBlocked(boolean isBlocked) {
+		this.isBlocked = isBlocked;
+	}
 }
